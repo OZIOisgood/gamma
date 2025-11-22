f53fa51e71e9e7102f3f7cb744468762239aae64
Lobarev Pavel <pashalobaryev111@gmail.com>
2025-11-22 13:33:51 +0100

    Load environment variables at startup and log an error if .env file is missing

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,9 +6,14 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+  if err := godotenv.Load(); err != nil {
+    log.Fatal("No .env file found")
+  }
+
   r := chi.NewRouter()
 
   r.Use(middleware.Logger)
